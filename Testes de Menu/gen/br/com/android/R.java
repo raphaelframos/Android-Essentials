d8ea5bf077b8e5f0e180aa67da6a93f208228530
Raphael Ramos <raphaelframos@gmail.com>
2012-06-11 22:44:20 -0300

    Menu funcionando

--- Testes de Menu/gen/br/com/android/R.java
+++ Testes de Menu/gen/br/com/android/R.java
@@ -13,8 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int item_dois=0x7f060001;
+        public static final int item_tres=0x7f060002;
+        public static final int item_um=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int menu_de_opcoes=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
