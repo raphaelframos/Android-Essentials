3150f99caa7bdaffa2f399321406d08bb90afaaf
Raphael Ramos <raphaelframos@gmail.com>
2012-06-16 21:05:08 -0300

    Issue 2

--- Issue/gen/br/com/androd/R.java
+++ Issue/gen/br/com/androd/R.java
@@ -14,13 +14,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int autoLink=0x7f050002;
-        public static final int btnBrowser=0x7f050003;
-        public static final int btnFundo=0x7f050001;
-        public static final int linearLayout=0x7f050000;
+        public static final int autoLink=0x7f050003;
+        public static final int btnBrowser=0x7f050004;
+        public static final int btnFundo=0x7f050002;
+        public static final int btnImagem=0x7f050005;
+        public static final int imageView1=0x7f050000;
+        public static final int linearLayout=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int exibe_imagem=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
