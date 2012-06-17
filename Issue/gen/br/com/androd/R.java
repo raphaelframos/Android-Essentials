a193f18039fa167f09c6228aaafd498faece876a
Raphael Ramos <raphaelframos@gmail.com>
2012-06-16 23:09:55 -0300

    Issue 6 sons soh nos botoes do main

--- Issue/gen/br/com/androd/R.java
+++ Issue/gen/br/com/androd/R.java
@@ -15,28 +15,31 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int autoLink=0x7f050004;
-        public static final int btnBrowser=0x7f050005;
-        public static final int btnFundo=0x7f050003;
-        public static final int btnImagem=0x7f050006;
-        public static final int btnMain=0x7f050001;
-        public static final int btnSoma=0x7f05000d;
-        public static final int btnSomaNumero=0x7f050007;
-        public static final int editText1=0x7f05000a;
-        public static final int editText2=0x7f05000c;
-        public static final int imgBtnImg=0x7f050000;
-        public static final int linearLayout=0x7f050002;
-        public static final int txtNum1=0x7f050009;
-        public static final int txtNum2=0x7f05000b;
-        public static final int txtResultado=0x7f050008;
+        public static final int autoLink=0x7f060004;
+        public static final int btnBrowser=0x7f060005;
+        public static final int btnFundo=0x7f060003;
+        public static final int btnImagem=0x7f060006;
+        public static final int btnMain=0x7f060001;
+        public static final int btnSoma=0x7f06000d;
+        public static final int btnSomaNumero=0x7f060007;
+        public static final int editText1=0x7f06000a;
+        public static final int editText2=0x7f06000c;
+        public static final int imgBtnImg=0x7f060000;
+        public static final int linearLayout=0x7f060002;
+        public static final int txtNum1=0x7f060009;
+        public static final int txtNum2=0x7f06000b;
+        public static final int txtResultado=0x7f060008;
     }
     public static final class layout {
         public static final int exibe_imagem=0x7f030000;
         public static final int main=0x7f030001;
         public static final int soma=0x7f030002;
     }
+    public static final class raw {
+        public static final int evil=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
